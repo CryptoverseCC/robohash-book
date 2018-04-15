35d96f9f11bdc683daa61214852a558d8f174c21
ghostbuster91 <kghost0@gmail.com>
2018-04-15 19:06:16 +0200

    Convert to robohash

--- src/entityApi.js
+++ src/entityApi.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import etherDiamond from './img/ether-diamond.gif';
+import Web3 from 'web3';
+const {
+  REACT_APP_ERC_721_ADDRESS: ERC_721_ADDRESS,
+} = process.env;
 
-const colors = {
-  babypuke: '#eff1e0',
-  bubblegum: '#fadff4',
-  chestnut: '#efe1da',
-  coralsunrise: '#fde9e4',
-  cyan: '#c5eefa',
-  doridnudibranch: '#faeefa',
-  eclipse: '#e5e7ef',
-  forgetmenot: '#dcebfc',
-  gold: '#faf4cf',
-  limegreen: '#d9f5cb',
-  mintgreen: '#cdf5d4',
-  parakeet: '#e5f3e2',
-  pumpkin: '#fae1ca',
-  sapphire: '#d3e8ff',
-  sizzurp: '#dfdffa',
-  strawberry: '#ffe0e5',
-  thundergrey: '#eee9e8',
-  topaz: '#d1eeeb',
-  twilightsparkle: '#ede2f5'
-};
+const roboHashTokenArtifacts = require('./abi/RoboHashToken.json');
+const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io'));
+const contractInstance = new web3.eth.Contract(roboHashTokenArtifacts, ERC_721_ADDRESS);
 
-export const getEntityData = async catId => {
+export const getEntityData = async entityId => {
   try {
-    const res = await fetch(`https://api.cryptokitties.co/kitties/${catId}`);
-    const data = await res.json();
-    data.color = colors[data.color];
-    data.url = `https://cryptokitties.co/kitty/${catId}`;
-    return data;
+    const responseTokenName = await contractInstance.methods.getTokenName(entityId).call();
+    const tokenName = responseTokenName.valueOf();
+    const tokenUrl = "http://robohash.org/" + tokenName + ".png";
+    return {
+      id: entityId,
+      name: tokenName,
+      image_url: tokenUrl, // image of our entity
+      url: `https://robohash.org`, // website with details about particular entity
+      color: '#333333' // background color 
+    };
   } catch (e) {
+    console.error(e);
     return undefined;
   }
 };
@@ -40,15 +32,15 @@ export const EntityIcon = entityId => {
 };
 
 export const entityTranslations = {
-  commentPlaceholder: 'Purr your story',
-  replyPlaceholder: 'Purr your reply',
-  noEntitiesError: 'No cats found',
-  entityName: 'Kitty'
+  commentPlaceholder: 'Hash your story',
+  replyPlaceholder: 'Hash your reply',
+  noEntitiesError: 'No robohashes found',
+  entityName: 'RoboHash'
 };
 
 export const avatarSizes = {
-  verySmall: { containerSize: '32px', imgSize: '70px', imgTopOffset: '85%', imgLeftOffset: '55%' },
-  small: { containerSize: '44px', imgSize: '110px', imgTopOffset: '85%', imgLeftOffset: '55%' },
-  medium: { containerSize: '54px', imgSize: '120px', imgTopOffset: '77%', imgLeftOffset: '55%' },
-  large: { containerSize: '64px', imgSize: '130px', imgTopOffset: '70%', imgLeftOffset: '55%' }
+  verySmall: { containerSize: '32px', imgSize: '32px', imgTopOffset: '50%', imgLeftOffset: '50%' },
+  small: { containerSize: '44px', imgSize: '44px', imgTopOffset: '50%', imgLeftOffset: '50%' },
+  medium: { containerSize: '54px', imgSize: '54px', imgTopOffset: '50%', imgLeftOffset: '50%' },
+  large: { containerSize: '64px', imgSize: '64px', imgTopOffset: '50%', imgLeftOffset: '50%' }
 };
